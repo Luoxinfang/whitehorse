ca729e1d9a8405bcfaf3b5be8f892314594afa9a
Administrator <xf.radish@gmail.com>
2015-02-02 00:28:21 +0800

    update login model

---
+++ app.js
@@ -0,0 +1,42 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+
+var app = express();
+// all environments
+app.set('env', 'development'); //production
+app.set('port', process.env.PORT || 3000, null);
+app.set('views', path.join(__dirname, 'views'), null);
+
+app.use(express.favicon('public/imgs/icon/somgle.ico'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+
+
+app.configure('production', function () {
+  app.use(express.static(path.join(__dirname, 'dist')));
+});
+app.configure('development', function () {
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.errorHandler());
+});
+
+
+http.createServer(app).listen(app.get('port'), function () {
+  var info = 'Express server start in ' + app.get('env') +
+    ' model & listening on port ' + app.get('port');
+  console.log(info);
+});
+
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
+});
+
+app.use(express.logger('dev'));
---
+++ public/js/page/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by roy on 2015/1/24.
+ */
+(function (angular) {
+  angular.module('somgle', []);
+
+})(window.angular);
